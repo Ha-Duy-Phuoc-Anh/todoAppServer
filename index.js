b280ac960c37ee0e4ecb907331593f9f5e250258
Phixah <nicknameabc4@gmail.com>
2025-10-23 11:14:14 +0700

    modified:   index.js

--- index.js
+++ index.js
@@ -82,6 +82,5 @@ app.delete("/todos/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("server has started on port 5000");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server started on port ${PORT}`));
